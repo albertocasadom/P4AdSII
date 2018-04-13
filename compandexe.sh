747e162e673aff2b8a5244358f7662c9204b4ae2
Alberto Casado Medina <albertocm@MacBook-Pro-de-Alberto.local>
2018-04-13 19:57:06 +0200

    Acabado funcionalidad sin hebras

---
+++ compandexe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear;
+g++ -o controlclientes controlclientes.cpp && ./controlclientes
+
+
+ 
--- controlclientes.cpp
+++ controlclientes.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define MAX_CLIENTES 25
+#define MAX_CLIENTES 3
 #define T_A 1
 #define T_B 2
 #define T_C 3
@@ -23,6 +23,7 @@ int menu(){
 
 	int option = 0;
 
+	cout << endl;
 	cout << "[1] Imprimir datos de clientes\n";
 	cout << "[2] Alta de usuario\n";
 	cout << "[3] Baja de usuario\n";
@@ -35,52 +36,96 @@ int menu(){
 	return option;
 }
 
+void imprimir_datos_cl(){
+
+	if(user == 0){
+		cout << endl << " No hay clientes registrados" << endl << endl;
+	}else{
+		for(int i = 0; i<user;i++){
+			cout << "[" << i+1 << "] DNI: " << datos_cl[i].dni << " | Nombre: " << datos_cl[i].nombre 
+			<< " | Tarifa: " << datos_cl[i].tarifa << " | Año de alta: " << datos_cl[i].alta << " | Descuento: "
+			<< datos_cl[i].descuento << endl;
+		}
+	}
+}
+
 int alta_usr(){
 
-	unsigned dni, alta, descuento;
-	string nombre;
-	char tarifa;
+	if (user < MAX_CLIENTES){ 
+		unsigned dni, alta, descuento;
+		string nombre;
+		char tarifa;
 
-	info_cliente_t new_user;
+		info_cliente_t new_user;
 
-	cout << "Introduzca DNI del cliente: ";
-	cin >> new_user.dni;
-	cout << "Introduzca nombre del cliente: ";
-	cin >> new_user.nombre;
-	cout << "Introduzca tarifa (A, B o C) del cliente: ";
-	cin >> new_user.tarifa;
-	cout << "Introduzca año de alta del cliente: ";
-	cin >> new_user.alta;
-	cout << "Introduzca descuento del cliente: ";
-	cin >> new_user.descuento;
+		cout << "Introduzca DNI del cliente: ";
+		cin >> new_user.dni;
+		cout << "Introduzca nombre del cliente: ";
+		cin >> new_user.nombre;
+		cout << "Introduzca tarifa (A, B o C) del cliente: ";
+		cin >> new_user.tarifa;
+		cout << "Introduzca año de alta del cliente: ";
+		cin >> new_user.alta;
+		cout << "Introduzca descuento del cliente: ";
+		cin >> new_user.descuento;
+		cout << endl;
+
+		for(int j = 0; j<MAX_CLIENTES; j++){
+			if(datos_cl[j].dni == new_user.dni){
+				cout << "El usuario ya está dado de alta" << endl << endl;
+				return -1;
+			}
+		}
+
+		datos_cl[user] = new_user;
+		user++;
+
+		return 0;
+
+	}else{
+		cout << endl << "Se ha llegado al número máximo de clientes" << endl;
+		return -1;
+	}
+}
+
+unsigned baja_usr(unsigned dni){
 
 	int i = 0;
-
-	for(int j = 0; j<MAX_CLIENTES; j++){
-		if(datos_cl[j].dni == dni){
-			cout << "El usuario ya está dado de alta" << endl;
-			return -1;
+	unsigned ok = 0;
+	while(i < user){
+		if(datos_cl[i].dni != dni){
+			i++;
+		}else{
+			for(int x = i; x<user; x++){
+				datos_cl[x] = datos_cl[x+1];
+				user--;
+				ok = 1;
+			}
 		}
 	}
 
-	datos_cl[user] = new_user;
-	user++;
-
-	return 0;
+	return ok;
 }
+unsigned cambiar_tarifa(unsigned dni, char tarifa){
 
-void imprimir_datos_cl(){
+	unsigned ok = 0;
 
-	cout << "   DNI   | Nombre  |	Tarifa 	 |  Alta  |Descuento " << endl;
-	for(int i = 0; i<user;i++){
-		cout << datos_cl[i].dni << " |" << datos_cl[i].nombre << " | " << datos_cl[i].tarifa << " | " 
-		<< datos_cl[i].alta << " | " << datos_cl[i].descuento << endl;
+	for(int i = 0; i< user; i++){
+		if(datos_cl[i].dni == dni){
+			datos_cl[i].tarifa = tarifa;
+			ok = 1;
+		}
 	}
+
+	return ok;
 }
 
 int main(){
 
 	int alta = 0;
+	unsigned dni;
+	unsigned ok;
+	char tarifa;
 
 		while (1){
 
@@ -97,8 +142,22 @@ int main(){
 					}
 				break;
 				case 3:
+					cout << "Introduzca el DNI del cliente que desee dar de baja: ";
+					cin >> dni;
+					ok = baja_usr(dni);
+					if(ok == 0){
+						cout << endl << "No existe ningún usuario con el DNI introducido." << endl;
+					}
 				break;
 				case 4:
+					cout << "Introduzca el DNI del cliente al que se le desea aplicar la nueva tarifa: ";
+					cin >> dni;
+					cout << endl << "Introduzca la nueva tarifa: ";
+					cin >> tarifa;
+					ok = cambiar_tarifa(dni, tarifa);
+					if(ok == 0){
+						cout << "No existe ningún usuario con el DNI introducido." << endl;
+					}
 				break;
 				case 5:
 				break;
